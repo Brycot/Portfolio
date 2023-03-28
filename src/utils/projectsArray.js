22626e1c1dda1a2bc4e23dae35428484220682b2
Bryan Ismael Figueroa Quiroz <bryalquiroz@gmail.com>
2023-03-28 17:30:20 -0500

    ✨[ADD] Add Project

--- src/utils/projectsArray.js
+++ src/utils/projectsArray.js
@@ -1,57 +1,77 @@
 const projectsArray = [];
 projectsArray.push({
-    name: "TifySpo",
-    demo: "",
-    git: "",
-    img: "https://i.imgur.com/X4lCN2d.png",
+    name: 'TifySpo',
+    demo: '',
+    git: '',
+    img: 'https://i.imgur.com/X4lCN2d.png',
     description:
-        "Clon de Spotify que usa la API de spotify para hacer la autenticacioón y asi poder acceder a las playlist/albumes del usuario y reproducir musica",
-    stack: ["HTML5", "CSS3", "JavaScript", "API-Rest"],
+        'Clon de Spotify que usa la API de spotify para hacer la autenticacioón y asi poder acceder a las playlist/albumes del usuario y reproducir musica',
+    stack: ['HTML5', 'CSS3', 'JavaScript', 'API-Rest'],
 });
 projectsArray.push({
-    name: "Petgram",
-    demo: "https://brycot-petgram.vercel.app/",
-    git: "https://github.com/Brycot/petgram",
-    img: "https://i.imgur.com/rejfegA.png",
+    name: 'YumYum',
+    demo: 'https://yumyum-bf.vercel.app/',
+    git: 'https://github.com/Brycot/YumYum',
+    img: 'https://i.imgur.com/5f3Gd75.png',
     description:
-        "Aplicación replica de instagram para mostrar imagenes de mascotas, con autenticación para guardar imagenes en favoritas podiendo registrarse e iniciar sesión",
-    stack: ["HTML5", "Styled-Components", "JavaScript", "React", "API-Rest"],
+        'Aplicación full-stack que permite buscar, filtrar y crear nuevas recetas de diferentes tipos de cocina, ingredientes y dietas. Las recetas guardadas se almacenan en la base de datos donde tienen una relación entidad, mucho a muchos donde su tipo de dietas. También se puede ver el detalle de cada receta, con los pasos a seguir, la calificación de salud e información general de la misma.',
+    stack: [
+        'HTML5',
+        'CSS-Modules',
+        'JavaScript',
+        'React',
+        'API-Rest',
+        'Redux',
+        'Node JS',
+        'Express Js',
+        'PosgreSQL',
+        'Sequelize',
+    ],
 });
 projectsArray.push({
-    name: "Music Sale",
-    demo: "https://brycot.github.io/music-shop/",
-    git: "https://github.com/Brycot/music-shop",
-    img: "https://i.imgur.com/BteT2rJ.png",
+    name: 'Petgram',
+    demo: 'https://brycot-petgram.vercel.app/',
+    git: 'https://github.com/Brycot/petgram',
+    img: 'https://i.imgur.com/rejfegA.png',
     description:
-        "Eccomerce digital de musica creado usando la libreria de facebook react, teniendo funcionalidades como agregar al carrito, detalle del producto, historia de ordenes y persistencia de datos con localStorage",
-    stack: ["HTML5", "SCSS", "JavaScript", "React", "API-Rest"],
+        'Aplicación replica de instagram para mostrar imagenes de mascotas, con autenticación para guardar imagenes en favoritas podiendo registrarse e iniciar sesión',
+    stack: ['HTML5', 'Styled-Components', 'JavaScript', 'React', 'API-Rest'],
 });
 projectsArray.push({
-    name: "Todo-Machine",
-    demo: "https://brycot.github.io/todo-machine/",
-    git: "https://github.com/Brycot/todo-machine",
-    img: "https://i.imgur.com/MPhbpMI.png",
+    name: 'Music Sale',
+    demo: 'https://brycot.github.io/music-shop/',
+    git: 'https://github.com/Brycot/music-shop',
+    img: 'https://i.imgur.com/BteT2rJ.png',
     description:
-        "Todo List creada con React usando localstorage para la persistencia de datis añadiendo funcionalides que no tuvo la primera versión que realice de esta app usando solo JavaScript vanilla, me sirvio en gran medida para practicar y mejorar mis habilidades y conocimientos",
-    stack: ["HTML5", "CSS3", "JavaScript", "React"],
+        'Eccomerce digital de musica creado usando la libreria de facebook react, teniendo funcionalidades como agregar al carrito, detalle del producto, historia de ordenes y persistencia de datos con localStorage',
+    stack: ['HTML5', 'SCSS', 'JavaScript', 'React', 'API-Rest'],
 });
 projectsArray.push({
-    name: "Rick & Morty",
-    demo: "https://rick-morty-lvl0n3t6c-brycot.vercel.app/",
-    git: "https://github.com/Brycot/rick-morty",
-    img: "https://i.imgur.com/qX79uyz.png",
+    name: 'Todo-Machine',
+    demo: 'https://brycot.github.io/todo-machine/',
+    git: 'https://github.com/Brycot/todo-machine',
+    img: 'https://i.imgur.com/MPhbpMI.png',
     description:
-        "Aplicación que hace uso de la rick&morty API para obtener el genero, estatus, origen y ultima ubicación de los personajes de la serie.",
-    stack: ["HTML5", "CSS3", "JavaScript", "React", "API-Rest"],
+        'Todo List creada con React usando localstorage para la persistencia de datis añadiendo funcionalides que no tuvo la primera versión que realice de esta app usando solo JavaScript vanilla, me sirvio en gran medida para practicar y mejorar mis habilidades y conocimientos',
+    stack: ['HTML5', 'CSS3', 'JavaScript', 'React'],
 });
 projectsArray.push({
-    name: "Condor-Replic",
-    demo: "https://brycot.github.io/condor-replic/",
-    git: "https://github.com/Brycot/condor-replic",
-    img: "https://i.imgur.com/dbAb2u3.png",
+    name: 'Rick & Morty',
+    demo: 'https://rick-morty-lvl0n3t6c-brycot.vercel.app/',
+    git: 'https://github.com/Brycot/rick-morty',
+    img: 'https://i.imgur.com/qX79uyz.png',
     description:
-        "Aplicación de gestion academica inspirada en la pagina academica de la universidad distrital de Francisco Jose de Caldas, utilizando POO para la creación de estudiantes, asignaturas y docentes, asi como una autorización y autenticación basica.",
-    stack: ["HTML5", "CSS3", "JavaScript"],
+        'Aplicación que hace uso de la rick&morty API para obtener el genero, estatus, origen y ultima ubicación de los personajes de la serie.',
+    stack: ['HTML5', 'CSS3', 'JavaScript', 'React', 'API-Rest'],
+});
+projectsArray.push({
+    name: 'Condor-Replic',
+    demo: 'https://brycot.github.io/condor-replic/',
+    git: 'https://github.com/Brycot/condor-replic',
+    img: 'https://i.imgur.com/dbAb2u3.png',
+    description:
+        'Aplicación de gestion academica inspirada en la pagina academica de la universidad distrital de Francisco Jose de Caldas, utilizando POO para la creación de estudiantes, asignaturas y docentes, asi como una autorización y autenticación basica.',
+    stack: ['HTML5', 'CSS3', 'JavaScript'],
 });
 // projectsArray.push({
 //     name: "ToDo-Js-Vanilla",
@@ -63,29 +83,29 @@ projectsArray.push({
 //     stack: ["HTML5", "CSS3", "JavaScript"],
 // });
 projectsArray.push({
-    name: "Mitio",
-    demo: "https://brycot.github.io/Mition/",
-    git: "https://github.com/Brycot/Mition",
-    img: "https://i.imgur.com/gy3ZZfS.png",
+    name: 'Mitio',
+    demo: 'https://brycot.github.io/Mition/',
+    git: 'https://github.com/Brycot/Mition',
+    img: 'https://i.imgur.com/gy3ZZfS.png',
     description:
-        "App web que consume al API de MovieDB para acceder a miles de peliculas asi como su información, calificación, reparto y su trailer. añadiendo el uso de tecnolias como LazyLoad",
-    stack: ["HTML5", "CSS3", "JavaScript", "API-Rest"],
+        'App web que consume al API de MovieDB para acceder a miles de peliculas asi como su información, calificación, reparto y su trailer. añadiendo el uso de tecnolias como LazyLoad',
+    stack: ['HTML5', 'CSS3', 'JavaScript', 'API-Rest'],
 });
 projectsArray.push({
-    name: "Fintech-Skeleton",
-    demo: "https://trusting-bose-317de8.netlify.app/",
-    git: "https://github.com/Brycot/Fintech-Skeleton",
-    img: "https://i.imgur.com/kbvfRls.png",
+    name: 'Fintech-Skeleton',
+    demo: 'https://trusting-bose-317de8.netlify.app/',
+    git: 'https://github.com/Brycot/Fintech-Skeleton',
+    img: 'https://i.imgur.com/kbvfRls.png',
     description:
-        "Maqueta de un dashboard de una aplicación fintech con diseño responsivo.",
-    stack: ["HTML5", "CSS3"],
+        'Maqueta de un dashboard de una aplicación fintech con diseño responsivo.',
+    stack: ['HTML5', 'CSS3'],
 });
 projectsArray.push({
-    name: "Netflix Clone HMTL",
-    demo: "https://netflix-brycot.netlify.app/",
-    git: "https://github.com/Brycot/netflix-clone-html",
-    img: "https://i.imgur.com/euC4HXV.png",
-    description: "Maqueta del formulario de inicio de sesión de netflix.",
-    stack: ["HTML5", "CSS3"],
+    name: 'Netflix Clone HMTL',
+    demo: 'https://netflix-brycot.netlify.app/',
+    git: 'https://github.com/Brycot/netflix-clone-html',
+    img: 'https://i.imgur.com/euC4HXV.png',
+    description: 'Maqueta del formulario de inicio de sesión de netflix.',
+    stack: ['HTML5', 'CSS3'],
 });
 export default projectsArray;
